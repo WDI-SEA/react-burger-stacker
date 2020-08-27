60bba0ac783561899c8390350292f88ae92d26d1
blangwell <b.langwell@outlook.com>
2020-08-27 16:30:00 -0700

    [ingredients] Add ingredients to IngredientList component and style with color values

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BurgerPane from './BurgerPane'
 import IngredientList from './IngredientList'
 
+let burgerIngredients = [
+  {name: 'Kaiser Bun', color: 'saddlebrown'},
+  {name: 'Sesame Bun', color: 'sandybrown'},
+  {name: 'Gluten Free Bun', color: 'peru'},
+  {name: 'Lettuce Wrap', color: 'olivedrab'},
+  {name: 'Beef Patty', color: '#3F250B'},
+  {name: 'Soy Patty', color: '#3F250B'},
+  {name: 'Black Bean Patty', color: '#3F250B'},
+  {name: 'Chicken Patty', color: 'burlywood'},
+  {name: 'Lettuce', color: 'lawngreen'},
+  {name: 'Tomato', color: 'tomato'},
+  {name: 'Bacon', color: 'maroon'},
+  {name: 'Onion', color: 'lightyellow'}
+]
+
 function App() {
+  const [burger, setBurger] = useState([])
   return (
     <div className="App">
-      <BurgerPane />
-      <IngredientList />
+      <BurgerPane ingredients={burgerIngredients}
+      userBurger={burger} />
+      <IngredientList ingredients={burgerIngredients} />
     </div>
   );
 }
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -3,10 +3,18 @@ import Ingredients from './Ingredients';
 import './App.css';
 
 
-export default function IngredientList() {
+export default function IngredientList(props) {
+    const allIngredients = props.ingredients.map(ing => {
+        return <li style={{color: ing.color}}>{ing.name}</li>
+    })
     return (
+
         <div className="ingredient-list-div">
             <h1>This the Ingredient List</h1>
+            {/* we'll need to use map to list all ingredients */}
+            <ul>
+                {allIngredients}
+            </ul>
         </div>
     )
 }
