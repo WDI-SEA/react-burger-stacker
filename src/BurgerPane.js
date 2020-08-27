e9d2e6fa848a0ba753bc6c6ef6fd447831fab79f
blangwell <b.langwell@outlook.com>
2020-08-27 15:56:44 -0700

    [components] Add BurgerPane and IngredientList components

---
+++ src/BurgerPane.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+
+export default function BurgerPane() {
+    return (
+        <div className="burger-pane-div">
+            <h1>This the Burger Pane</h1>
+        </div>
+    )
+}
---
+++ src/IngredientList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+
+
+export default function IngredientList() {
+    return (
+        <div className="ingredient-list-div">
+            <h1>This the Ingredient List</h1>
+        </div>
+    )
+}
