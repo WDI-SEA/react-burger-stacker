2aa26882f58a79a6cd2cbe837a445a02482605f5
Lizz <lizwesterband@gmail.com>
2020-08-27 15:48:55 -0700

    [first] First commit; add files for Functional Components

---
+++ src/ClearBurger.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ClearBurger = (props) => {
+
+}
+
+export default ClearBurger
---
+++ src/IngredientList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const IngredientList = (props) => {
+
+}
+
+export default IngredientList
