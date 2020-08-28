530e632c5afd7a0e181c8e4724f0fa55dc718fa0
yoel <yoel@macbook-pro.home>
2020-08-27 22:55:55 -0700

    [react-burger-stacker] add switch cases, emojis and onClick function

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -9,6 +9,46 @@ export default class Burger extends Component {
     meat: 0
   }
 
+  addRemoveIngredient = (action, ingredient) => {
+      let {
+        lettuce,
+        tomato,
+        cheese,
+        meat
+      } = this.state;
+
+      let stateValue;
+
+      switch(ingredient){
+        case 'lettuce':{
+          stateValue = lettuce;
+          break;
+        }
+        case 'tomato':{
+          stateValue = tomato;
+          break;
+        }
+        case 'cheese':{
+          stateValue = cheese;
+          break;
+        }
+        case 'meat':{
+          stateValue = meat;
+          break;
+        }
+          default: break;
+      }
+      
+      if(action === 'add'){
+          stateValue = stateValue + 1
+      } else {
+          stateValue = stateValue - 1;
+      }
+      this.setState({
+          [ingredient]: stateValue >= 0 ? stateValue : 0
+      });
+  }
+
   render() {
     return (
       <>
@@ -16,10 +56,25 @@ export default class Burger extends Component {
               Burger
           </div>
           <div className="ingredientsBlock">
-              <p>Lettuce</p>
+              <p>Lettuce 🥬</p>
               <div className="ingrBtns">
               <button onClick={() => this.addRemoveIngredient('add', 'lettuce')} className="ingrBtn">Add</button>
               <button onClick={() => this.addRemoveIngredient('remove', 'lettuce')} className="ingrBtn">Remove</button>
+              </div>
+              <p>Tomato 🍅</p>
+              <div className="ingrBtns">
+              <button onClick={() => this.addRemoveIngredient('add', 'tomato')} className="ingrBtn">Add</button>
+              <button onClick={() => this.addRemoveIngredient('remove', 'tomato')} className="ingrBtn">Remove</button>
+              </div>
+              <p>Cheese 🧀</p>
+              <div className="ingrBtns">
+              <button onClick={() => this.addRemoveIngredient('add', 'cheese')} className="ingrBtn">Add</button>
+              <button onClick={() => this.addRemoveIngredient('remove', 'cheese')} className="ingrBtn">Remove</button>
+              </div>
+              <p>Meat 🍖</p>
+              <div className="ingrBtns">
+              <button onClick={() => this.addRemoveIngredient('add', 'meat')} className="ingrBtn">Add</button>
+              <button onClick={() => this.addRemoveIngredient('remove', 'meat')} className="ingrBtn">Remove</button>
               </div>
           </div>
       </>
