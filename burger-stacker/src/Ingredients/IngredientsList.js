64f779dd30286f1bd3276b9ce3193c9f5c50df94
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-28 02:22:20 -0700

    [react] use hooks

--- burger-stacker/src/Ingredients/IngredientsList.js
+++ burger-stacker/src/Ingredients/IngredientsList.js
@@ -1,47 +1,25 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Ingredients from '../Ingredients/Ingredients'
 import BurgerStack from '../Burger/BurgerStack';
 
-class IngredientsList extends Component {
+let IngredientsList = (props) => {
 
-    state = {
-        ingredientsArray: this.props.ingredients,
+    const[burger, setBurger] = useState([])
+
+
+    const addIngredients = (e, ingredients) => {
+        e.preventDefault()
+        setBurger(burger.concat(ingredient))
     }
-
-    addIngredient = e => {
-        console.log(e)
-
-        let addIngredient=this.state.addIngredient;
-        this.setState({
-            addIngredient: this.state.ingredientAdd
-        })
-    }
-
-    toBurger = e => {
-        this.setState({
-            addIngredient: this.state.ingredientAdd
-        })
-    }
-
-    render() {
-
-        let ingredientsList = this.state.ingredientsArray.map( (item, index) => (
-            <>
-            <Ingredients ingredients={item} key={index} />
-            <input hidden type="text" name="ingredientAdd" value={this.state.ingredientAdd}></input>
-            <button onClick={(e) => this.addIngredient(e)}>+</button>
-            </>
-        ))
 
         return (
             <div>
                 <h1>Ingredients</h1>
-                <ul>{ingredientsList}</ul>
+                {/* <ul>{ingredientsList}</ul> */}
                 <h1>Burger</h1>
                 {/* <ul>{burgerList}</ul> */}
             </div>
         )
-    }
 }
 
 export default IngredientsList
