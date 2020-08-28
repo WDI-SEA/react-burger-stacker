7e54743c92b910d8d912ba6eb78926991dc5ea53
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-08-28 09:53:15 -0600

    [bonus]set up bonuses

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import ListArea from './ListArea'
 import StackArea from './StackArea'
 
 
-let ingredients = [
-    {name: 'Kaiser Bun', color: 'saddlebrown'},
-    {name: 'Sesame Bun', color: 'sandybrown'},
-    {name: 'Gluten Free Bun', color: 'peru'},
-    {name: 'Lettuce Wrap', color: 'olivedrab'},
-    {name: 'Beef Patty', color: '#3F250B'},
-    {name: 'Soy Patty', color: '#3F250B'},
-    {name: 'Black Bean Patty', color: '#3F250B'},
-    {name: 'Chicken Patty', color: 'burlywood'},
-    {name: 'Lettuce', color: 'lawngreen'},
-    {name: 'Tomato', color: 'tomato'},
-    {name: 'Bacon', color: 'maroon'},
-    {name: 'Onion', color: 'lightyellow'}
-  ]
-
 function App() {
 
     const [burger, setBurger] = useState([])
+    const [ingredients, setIngredients] = useState(
+        [
+        {name: 'Kaiser Bun', color: 'saddlebrown'},
+        {name: 'Sesame Bun', color: 'sandybrown'},
+        {name: 'Gluten Free Bun', color: 'peru'},
+        {name: 'Lettuce Wrap', color: 'olivedrab'},
+        {name: 'Beef Patty', color: '#3F250B'},
+        {name: 'Soy Patty', color: '#3F250B'},
+        {name: 'Black Bean Patty', color: '#3F250B'},
+        {name: 'Chicken Patty', color: 'burlywood'},
+        {name: 'Lettuce', color: 'lawngreen'},
+        {name: 'Tomato', color: 'tomato'},
+        {name: 'Bacon', color: 'maroon'},
+        {name: 'Onion', color: 'lightyellow'}
+      ])
 
     const addIngredient = (e, ingredient) => {
         e.preventDefault()
@@ -38,11 +38,22 @@ function App() {
         setBurger()
     }
 
+    const addNewIngredient = (newIngred) => {
+        newIngred.preventDefault()
+        setIngredients(...ingredients, newIngred)
+    }
+
     return (
-        <div className="App">
+        <div className="container">
             <h1>Burger Stackin' Action</h1>
-            <ListArea ingredients={ingredients} onClick={addIngredient} />
-            <StackArea burger={burger} onClick={clearBurger} undo={undoBurger} />
+            <div className="App">
+                <div className="column">
+                    <ListArea ingredients={ingredients} onClick={addIngredient} addNew={addNewIngredient}/>
+                </div>
+                <div className="column">
+                    <StackArea burger={burger} onClick={clearBurger} undo={undoBurger} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/ListArea.js
+++ src/ListArea.js
@@ -3,15 +3,9 @@ import React from 'react'
 const ListArea = (props) => {
 
     const ingredients = props.ingredients.map((ingredient, i) => {
-        return <li key={i}
-                    style={{background: ingredient.color}}
-                    >
+        return <li key={i} style={{background: ingredient.color}} >
                     {ingredient.name} 
-                    <button 
-                    onClick={(e)=>props.onClick(e, ingredient)}
-                    >
-                    +
-                    </button>
+                    <button onClick={(e)=>props.onClick(e, ingredient)} >+</button>
                 </li>
     })
 
@@ -19,6 +13,12 @@ const ListArea = (props) => {
         <div className="container">
             <h2>Ingredients</h2>
             <ul> {ingredients} </ul>
+            <form>
+                <input type="text"
+                placeholder="Type a new item here"
+                />
+                <button onClick={props.addNew}>Add it!</button>
+            </form>
         </div>
     )
 }
