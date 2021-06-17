e72817128bd66a54425f7ddbe43fd391a5c26c58
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-17 18:57:25 -0500

    Added ingredient list, rendered, styled with respective colors.

--- src/IngList.jsx
+++ src/IngList.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import Ings from './Ings'
 
+const ingData = [
+    {name: 'Kaiser Bun', color: 'saddlebrown'},
+    {name: 'Sesame Bun', color: 'sandybrown'},
+    {name: 'Gluten Free Bun', color: 'peru'},
+    {name: 'Lettuce Wrap', color: 'olivedrab'},
+    {name: 'Beef Patty', color: '#3F250B'},
+    {name: 'Soy Patty', color: '#3F250B'},
+    {name: 'Black Bean Patty', color: '#3F250B'},
+    {name: 'Chicken Patty', color: 'burlywood'},
+    {name: 'Lettuce', color: 'lawngreen'},
+    {name: 'Tomato', color: 'tomato'},
+    {name: 'Bacon', color: 'maroon'},
+    {name: 'Onion', color: 'lightyellow'}
+]
+
 const IngList = () => {
+    const ingredients = ingData.map((e, index) => {
+        return <Ings
+            name={e.name}
+            color={e.color}
+            key={index}
+        />
+    })
     return (
         <div>
             <h2>Hello from IngList!</h2>
-            <Ings />
+            {ingredients}
         </div>
     )
 }
--- src/Ings.jsx
+++ src/Ings.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Ings = () => {
+const Ings = (props) => {
     return (
         <div>
             <h3>Hello from Ings!</h3>
+                <p style={{color: props.color}}>Name: {props.name}</p>
+                <p style={{color: props.color}}>Color: {props.color}</p>
         </div>
     )
 }
