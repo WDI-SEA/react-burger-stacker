34f70f8e0a50fbcd1e720c263716956fef556e3d
hamazumi <khamamoto12@gmail.com>
2021-06-17 17:05:35 -0700

    Added Components

---
+++ src/BurgerPane.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class BurgerPane extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BurgerPane
---
+++ src/IngredientList.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class IngredientList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default IngredientList
