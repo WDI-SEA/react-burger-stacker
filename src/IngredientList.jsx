f00ea109982ebb6b85547d53566864336f04bf31
Consecratio <jcasique@gmail.com>
2021-06-21 11:24:04 -0700

    updated to match codealong with Nick

--- src/IngredientList.jsx
+++ src/IngredientList.jsx
@@ -1,35 +1,25 @@
 import React, { Component } from 'react'
 import Ingredients from './Ingredients'
 
-const ingredients = [
-    {name: 'Kaiser Bun', color: 'saddlebrown'},
-    {name: 'Sesame Bun', color: 'sandybrown'},
-    {name: 'Gluten Free Bun', color: 'peru'},
-    {name: 'Lettuce Wrap', color: 'olivedrab'},
-    {name: 'Beef Patty', color: '#3F250B'},
-    {name: 'Soy Patty', color: '#3F250B'},
-    {name: 'Black Bean Patty', color: '#3F250B'},
-    {name: 'Chicken Patty', color: 'burlywood'},
-    {name: 'Lettuce', color: 'lawngreen'},
-    {name: 'Tomato', color: 'tomato'},
-    {name: 'Bacon', color: 'maroon'},
-    {name: 'Onion', color: 'lightyellow'}
-]
-
 export default class IngredientList extends Component {
     render() {
-        const ingredBuilder = ingredients.map((ingredient, index) => {
-            return <Ingredients 
-                name={ ingredient.name }
-                color={ ingredient.color }
-                key={ index }
-            />
+        // map over array of ingredients
+        let ingredientComponents = this.props.ingredients.map((ingredient, index) => {
+            return ( 
+                <li>
+                    <Ingredients 
+                        name={ ingredient.name }
+                        color={ ingredient.color }
+                        key={ index }
+                    />
+                </li>
+            )
         })
+
         return (
-            <div>
-                <p>Hello from IngredientList.jsx</p>
-                { ingredBuilder }
-            </div>
+            <ul>
+                { ingredientComponents }
+            </ul>
         )
     }
 }
--- src/Ingredients.jsx
+++ src/Ingredients.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Ingredients extends Component {
     render() {
         return (
-            <p style={{backgroundColor: this.props.color }}>{ this.props.name }</p>
+            <p style={{backgroundColor: this.props.color }}>
+                { this.props.name }
+            </p>
         )
     }
 }
