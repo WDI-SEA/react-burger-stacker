25d26434ccb5f2de34bc9710d6ebf1587544111e
erikwinkel <erikpwinkel@gmail.com>
2021-06-24 17:12:37 -0700

    fix reversing bug

--- src/BurgerStack.js
+++ src/BurgerStack.js
@@ -1,11 +1,11 @@
 import Ingredient from './Ingredient'
 
 const BurgerStack = (props) => {
-    let ingredients = props.ingredients.reverse().map((item,index) => (
+    let ingredients = props.ingredients.map((item,index) => (
         <li>
             <Ingredient ingredient={item.name} color={item.color} key={ index }/>
         </li>
-    ))
+    )).reverse()
 
     return (
         <ul>
