69ad5c2c09a899cfde8060d3859db689626235ad
jmeier2715 <jmeier2715@gmail.com>
2021-12-01 10:48:00 -0500

    working deliverable

--- burger-stacker/src/index.js
+++ burger-stacker/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import IngredientsList from './IngredientsList';
+import BurgerStacker from './components/BurgerStacker';
 import reportWebVitals from './reportWebVitals';
 
-const ingredients = [
-  {name: 'Kaiser Bun', color: 'saddlebrown'},
-  {name: 'Sesame Bun', color: 'sandybrown'},
-  {name: 'Gluten Free Bun', color: 'peru'},
-  {name: 'Lettuce Wrap', color: 'olivedrab'},
-  {name: 'Beef Patty', color: '#3F250B'},
-  {name: 'Soy Patty', color: '#3F250B'},
-  {name: 'Black Bean Patty', color: '#3F250B'},
-  {name: 'Chicken Patty', color: 'burlywood'},
-  {name: 'Lettuce', color: 'lawngreen'},
-  {name: 'Tomato', color: 'tomato'},
-  {name: 'Bacon', color: 'maroon'},
-  {name: 'Onion', color: 'lightyellow'}
-]
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>Welcome to Burger-Burg</h1>
-    <IngredientsList ingredients = {ingredients}/>
+    <BurgerStacker />
+    {/* <IngredientsList ingredients = {ingredients}/> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
