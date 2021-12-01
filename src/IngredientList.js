7784a113c626f3c778ab5d45fbf5c703e7d3c0bf
steven43533 <steven43533@gmail.com>
2021-11-30 20:15:44 -0500

    Added two containers for panels

---
+++ src/IngredientList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Ingredients extends Component {
+
+    
+
+    render() {
+        return(
+            <div className="container">
+                Container 1
+            </div>
+        )
+
+    }
+}
