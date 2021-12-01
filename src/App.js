5d94db800a22c3bbf7f5302cca9ea363ff91b3de
isaac8069 <isaac8069@gmail.com>
2021-12-01 10:06:52 -0500

    Burger Pane and Ingredient List added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BurgerStacker from './Components/BurgerStacker';
+
+function App() {
+  return (
+    <div className="App">
+      <BurgerStacker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BurgerPane.js
@@ -0,0 +1,20 @@
+// this is where the burger happens
+import React, { Component } from 'react'
+import Ingredient from './Ingredient'
+// we'll also want something that clears the burger
+
+
+export default class BurgerPane extends Component {
+    // we want to loop over the burgerbits that the user has added
+    // display one Ingredient component for every bit of the burger
+    render () {
+        let burgerBits = this.props.ingredients.map(mapIngredient => {
+            <Ingredient ingredient={mapIngredient} />
+        })
+        return (
+        <section className="pane">
+            <h3>The Burger</h3>
+            { burgerBits }
+        </section>)
+    }
+}
