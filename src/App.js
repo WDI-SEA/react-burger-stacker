f1aa414519c8fc3f9a7688fc639acbcaa97db4c4
DG-98 <dgarconpro@gmail.com>
2021-12-01 22:48:46 -0500

    completed deliverable with code along

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BurgerStacker from './components/BurgerStacker';
+import IngredientList from './components/IngredientList';
+import Ingredient from './components/Ingredient';
+
+function App() {
+  return (
+    <div className="App">
+     <BurgerStacker />
+    </div>
+  );
+}
+
+export default App;
