0ad6d19bf13e9d7bb2eabcc6a0f627a6be60b2fe
paulinal3 <paulinal3@outlook.com>
2021-12-03 17:03:57 -0500

    BurgerStacker refactored

--- src/components/BurgerStacker.js
+++ src/components/BurgerStacker.js
@@ -1,59 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import BurgerPane from './BurgerPane'
-import Ingredients from './Ingredient'
+// import Ingredients from './Ingredient'
 import IngredientList from './IngredientList'
 
-export default class BurgerStacker extends Component {
-    state = {
-        ingredients: [
-            {name: 'Kaiser Bun', color: 'saddlebrown'},
-            {name: 'Sesame Bun', color: 'sandybrown'},
-            {name: 'Gluten Free Bun', color: 'peru'},
-            {name: 'Lettuce Wrap', color: 'olivedrab'},
-            {name: 'Beef Patty', color: '#3F250B'},
-            {name: 'Soy Patty', color: '#3F250B'},
-            {name: 'Black Bean Patty', color: '#3F250B'},
-            {name: 'Chicken Patty', color: 'burlywood'},
-            {name: 'Lettuce', color: 'lawngreen'},
-            {name: 'Tomato', color: 'tomato'},
-            {name: 'Bacon', color: 'maroon'},
-            {name: 'Onion', color: 'lightyellow'},
-            {name: 'Cheese', color: 'gold'}
-          ],
-          burgerIngredients: []
-    }
+// export default class BurgerStacker extends Component {
+//     state = {
+//         ingredients: [
+//             {name: 'Kaiser Bun', color: 'saddlebrown'},
+//             {name: 'Sesame Bun', color: 'sandybrown'},
+//             {name: 'Gluten Free Bun', color: 'peru'},
+//             {name: 'Lettuce Wrap', color: 'olivedrab'},
+//             {name: 'Beef Patty', color: '#3F250B'},
+//             {name: 'Soy Patty', color: '#3F250B'},
+//             {name: 'Black Bean Patty', color: '#3F250B'},
+//             {name: 'Chicken Patty', color: 'burlywood'},
+//             {name: 'Lettuce', color: 'lawngreen'},
+//             {name: 'Tomato', color: 'tomato'},
+//             {name: 'Bacon', color: 'maroon'},
+//             {name: 'Onion', color: 'lightyellow'},
+//             {name: 'Cheese', color: 'gold'}
+//           ],
+//           burgerIngredients: []
+//     }
+
+const BurgerStacker = () => {
+
+    const [ingredients, setIngredients] = useState([
+        { name: 'Kaiser Bun', color: 'saddlebrown' },
+        { name: 'Sesame Bun', color: 'sandybrown' },
+        { name: 'Gluten Free Bun', color: 'peru' },
+        { name: 'Lettuce Wrap', color: 'olivedrab' },
+        { name: 'Beef Patty', color: '#3F250B' },
+        { name: 'Soy Patty', color: '#3F250B' },
+        { name: 'Black Bean Patty', color: '#3F250B' },
+        { name: 'Chicken Patty', color: 'burlywood' },
+        { name: 'Lettuce', color: 'lawngreen' },
+        { name: 'Tomato', color: 'tomato' },
+        { name: 'Bacon', color: 'maroon' },
+        { name: 'Onion', color: 'lightyellow' },
+        { name: 'Cheese', color: 'gold' }
+    ])
+    const [burgerIngredients, setBurgerIngredients] = useState([])
 
     // add to stack function
-    addToStack = (e) => {
+    const addToStack = (e) => {
         console.log('this was clicked\n', e.target)
         let ingColor = e.target.style.backgroundColor
         let ingName = e.target.innerText
-        this.setState({
-            burgerIngredients: [{name: ingName, color: ingColor}, ...this.state.burgerIngredients]
-        })
+        setBurgerIngredients(prevBurgIng => [{ name: ingName, color: ingColor }, ...burgerIngredients])
     }
 
-    clearBurger = () => {
-        this.setState({
-            burgerIngredients: []
-        })
+    const clearBurger = () => {
+        setBurgerIngredients([])
     }
 
-    render() {
-        return (
-            <main>
-                <h1>Burger Stacker</h1>
-                <div className='panes'>
-                    <IngredientList 
-                        ingredients={this.state.ingredients}
-                        add={this.addToStack}
-                    />
-                    <BurgerPane 
-                        ingredients={this.state.burgerIngredients}
-                        clear={this.clearBurger}
-                    />
-                </div>
-            </main>
-        )
-    }
+    return (
+        <main>
+            <h1>Burger Stacker</h1>
+            <div className='panes'>
+                <IngredientList
+                    ingredients={ingredients}
+                    add={addToStack}
+                />
+                <BurgerPane
+                    ingredients={burgerIngredients}
+                    clear={clearBurger}
+                />
+            </div>
+        </main>
+    )
 }
+
+export default BurgerStacker
