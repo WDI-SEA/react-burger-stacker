be0a0d929a34b0e1f0cee3ebc7c70a9ef98c4356
paulinal3 <paulinal3@outlook.com>
2021-12-03 17:08:56 -0500

    IngredientList refactored

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -2,22 +2,22 @@
 import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 
-export default class IngredientList extends Component {
-    render() {
-        // here we need to store our group of ingredients(the component)
-        let allIngredients = this.props.ingredients.map(mapIng => (
-            <li onClick={this.props.add}>
-                <Ingredient ingredient={mapIng} />
-            </li>
-        ))
-        // then in our return, we'll render those as a necessary
-        return (
-            <section className='pane'>
-                <h3>Ingredients:</h3>
-                <ul className='ingredient-list'>
-                    { allIngredients }
-                </ul>
-            </section>
-        )
-    }
+const IngredientList = props => {
+    // here we need to store our group of ingredients(the component)
+    let allIngredients = props.ingredients.map(mapIng => (
+        <li onClick={props.add}>
+            <Ingredient ingredient={mapIng} />
+        </li>
+    ))
+    // then in our return, we'll render those as a necessary
+    return (
+        <section className='pane'>
+            <h3>Ingredients:</h3>
+            <ul className='ingredient-list'>
+                { allIngredients }
+            </ul>
+        </section>
+    )
 }
+
+export default IngredientList
