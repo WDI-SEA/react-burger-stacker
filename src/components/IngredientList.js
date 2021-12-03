29d27621c60e2d4a6f729e590b4b4462e122731f
hectorzaragoza <zaragoza09@gmail.com>
2021-12-03 17:36:20 -0600

    converted IngredientList to functional components successfully

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,12 +1,11 @@
 // IngredientList will loop over props(ingredients) 
 // produces an Ingredient component for every ingredient in the array
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Ingredient from './Ingredient'
-export default class IngredientList extends Component {
-    render() {
+function IngredientList (props) {
         // here we need to store our group of ingredients(the component)
-        let allIngredients = this.props.ingredients.map(mapIngredient => (
-            <li onClick={this.props.add}>
+        let allIngredients = props.ingredients.map(mapIngredient => (
+            <li onClick={props.add}>
                 <Ingredient ingredient={mapIngredient} />
             </li>
         ))
@@ -19,5 +18,6 @@ export default class IngredientList extends Component {
                 </ul>
             </section>
         )
-    }
 }
+
+export default IngredientList
