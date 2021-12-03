f2daae767b72607524b9044b55880e22a1e15d23
devinrbopp <devinrbopp@gmail.com>
2021-12-03 15:17:01 -0700

    converted to hooks

--- burger-stacker/src/components/BurgerPane.js
+++ burger-stacker/src/components/BurgerPane.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Ingredient from './Ingredient'
 
-export default class BurgerPane extends Component {
+const BurgerPane = props => {
     // loop over burger bits
     // display one Ingredient component for every bit of the burger
-    render () {
-        let burgerIngredients = this.props.ingredients.map(mapIngredient => (
-            // we use a different function here assigned to clicky!! this is how we use the same component with different onClick functions
-            <Ingredient ingredient={mapIngredient} clicky={this.props.remove}/>
-        ))
-        return (
-            <section className='pane'>
-                <h3>Burger Goes Here:</h3>
-                { burgerIngredients }
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        )
-    }
+    let burgerIngredients = props.ingredients.map(mapIngredient => (
+        // we use a different function here assigned to clicky!! this is how we use the same component with different onClick functions
+        <Ingredient ingredient={mapIngredient} clicky={props.remove}/>
+    ))
+    return (
+        <section className='pane'>
+            <h3>Burger Goes Here:</h3>
+            { burgerIngredients }
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    )
 }
+
+export default BurgerPane
--- burger-stacker/src/components/Ingredient.js
+++ burger-stacker/src/components/Ingredient.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Ingredient extends Component {
-    
-    render () {
-        return (
-            // clicky means something different when it is rendered in the burger and when it is rendered on the ingredients menu
-            // it is still the same element!!
-            <p style={{backgroundColor: this.props.ingredient.color}} onClick={this.props.clicky}>
-                { this.props.ingredient.name }
-            </p>
-        )
-    }
+const Ingredient = props => {
+    return (
+        // clicky means something different when it is rendered in the burger and when it is rendered on the ingredients menu
+        // it is still the same element!!
+        <p style={{backgroundColor: props.ingredient.color}} onClick={props.clicky}>
+            { props.ingredient.name }
+        </p>
+    )
 }
+
+export default Ingredient
