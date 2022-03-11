a2d2cb6f19762fd9bddd49286896625298344dc8
Professorsurf <guevara.a.gabriel@gmail.com>
2022-03-11 09:16:05 -0800

    2-10 deliverable

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import IngredientList from './IngredientList';
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <div>
+
+        <h1>hello from component</h1>
+        
+      </div>
+    );
+  }
+}
+ 
+export default App;
