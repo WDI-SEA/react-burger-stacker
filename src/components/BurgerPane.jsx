f1323af85cd43f20649c744785f275e9cfca1aac
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-10 10:36:43 -0700

    Tailwind CSS added

--- src/components/BurgerPane.jsx
+++ src/components/BurgerPane.jsx
@@ -11,7 +11,7 @@ class BurgerPane extends Component {
         return (
         <div className="cols-start-2" >
             <h2 className='text-xl row-start-2' >BurgerPane</h2>
-                <button className="bg-gray-400 border-gray-600 p-2"
+                <button className="bg-gray-400 border-gray-600 p-2 border-solid border-2 border-black-500"
                     onClick={this.props.handleRemoveFromStack} >
                         Clear Stack
                 </button>
