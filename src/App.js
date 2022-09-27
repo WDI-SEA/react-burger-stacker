b80ec39c759ec2f88c202d0b29bf27e5d3dee438
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-26 22:40:53 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import IngredientLst from './IngredientList';
+import BurgerPane from './BurgerPane';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola senor</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/BurgerPane.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import IngredientLst from './IngredientList';
+
+export default class BugerPane extends Component {
+
+}
