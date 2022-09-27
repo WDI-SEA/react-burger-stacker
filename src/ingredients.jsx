ea0d06984c00d715aa8231e56beec2f3f9f901cd
syorn96 <syorn2014@gmail.com>
2022-09-26 18:21:52 -0700

    1.5hrs inbreaktime

--- src/ingredients.jsx
+++ src/ingredients.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 
 export default class Ingredients extends Component {
+    handleChange = () => {
+
+    }
   render(){
     return(
-        <button>{this.props.ingredient}</button>
+        <button style={{color: this.props.color}} onClick={this.handleChange()} value={this.props.ingredient}>{this.props.ingredient}</button>
     )
   }
 }
