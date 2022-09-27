048903db07647fea08458f25dd351cd68ad4fa02
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-09-26 17:36:12 -0700

    setup base components

---
+++ src/IngredientList.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+export default class IngredientList extends Component {
+    render() {
+        return
+    }
+}
---
+++ src/Ingredients.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+export default class Ingredients extends Component {
+    render() {
+        return
+    }
+}
