9c8c69140d294f41b6b907cff9fc16bc8f41f1b7
weston-bailey <wdbailey87@gmail.com>
2022-09-27 10:28:11 -0700

    pushing code

---
+++ src/ClearBurger.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClearBurger extends Component {
+    render() {
+        return (
+            <div>
+                ClearBurger
+            </div>
+        )
+    }
+}
---
+++ src/IngredientList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Ingredient from './Ingredient'
+
+
+export default class IngredientList extends Component {
+    render() {
+        const ingredientComponents = this.props.ingredients.map((item, i) => {
+            return (
+                <Ingredient 
+                    // pass in the click event handler
+                    ingredient={item}
+                    handleIngredientClick={this.props.handleIngredientClick}
+                    key={`ingredient-list-${i}`}
+                />
+            )
+        })
+
+        // ingredientComponents.push(<Ingredient ingredient={{ name: 'American Cheese', color: 'yellow' }} key={`ingrediant-list${ingredientComponents.length}`}  />)
+
+        return (
+            <div>
+                {ingredientComponents}
+            </div>
+        )
+    }
+}
