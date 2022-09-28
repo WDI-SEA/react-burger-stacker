bee3b97248c84f4c8fa32803e21f5d1057b9cb59
weston-bailey <wdbailey87@gmail.com>
2022-09-28 16:47:36 -0700

    refactors to functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import { useState } from 'react'
 import IngredientList from './IngredientList'
 import BurgerPane from './BurgerPane'
 
@@ -17,49 +18,77 @@ const ingredients = [
   {name: 'Onion', color: 'lightyellow'}
 ]
 
-export default class App extends Component {
-  // have state that keeps track of clicked ingredients in an array
-  state = {
-    // array of ingredients that the user has clicked on
-    clickedIngredients: []
+export default function App() {
+  const [clickedIngredients, setClickedIngredients] = useState([])
+
+  const handleIngredientClick = ingredient => {
+    setClickedIngredients([ingredient, ...clickedIngredients])
   }
 
-  // click even handler -- when an ingredient is click, it will add that ingredient to that array in state of clicked ingredients
-  handleIngredientClick = ingredient => {
-    // console.log(e.target.innerText)
-    console.log(ingredient)
-    this.setState(prevState => {
-      // add the ingredient that was clicked to a new array with the old state's array spred into it
-      const clickedIngredients = [ingredient, ...prevState.clickedIngredients]
-      // merge the new array into state
-      return {
-        clickedIngredients
-      }
-    })
+  const handleBurgerClear = () => {
+    setClickedIngredients([])
   }
 
-  // clear button click handler, which will empty the array in state
-  handleBurgerClear = () => {
-    this.setState({
-      clickedIngredients: []
-    })
-  }
-
-  render() {
-    return (
+  return (
       <div style={{ display: 'flex', margin: '3rem', alignItems: 'flex-end' }}>
         {/* will recieve ingredients a props and render them, and a event handler to handle ingredient clicks */}
         <IngredientList 
           ingredients={ingredients}
-          handleIngredientClick={this.handleIngredientClick}
+          handleIngredientClick={handleIngredientClick}
         />
 
         {/* will recive the clear button event handler, and the clicked ingredients from state to render */}
         <BurgerPane 
-          clickedIngredients={this.state.clickedIngredients}
-          handleBurgerClear={this.handleBurgerClear}
+          clickedIngredients={clickedIngredients}
+          handleBurgerClear={handleBurgerClear}
         />
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+// export default class App extends Component {
+//   // have state that keeps track of clicked ingredients in an array
+//   state = {
+//     // array of ingredients that the user has clicked on
+//     clickedIngredients: []
+//   }
+
+//   // click even handler -- when an ingredient is click, it will add that ingredient to that array in state of clicked ingredients
+//   handleIngredientClick = ingredient => {
+//     // console.log(e.target.innerText)
+//     console.log(ingredient)
+//     this.setState(prevState => {
+//       // add the ingredient that was clicked to a new array with the old state's array spred into it
+//       const clickedIngredients = [ingredient, ...prevState.clickedIngredients]
+//       // merge the new array into state
+//       return {
+//         clickedIngredients
+//       }
+//     })
+//   }
+
+//   // clear button click handler, which will empty the array in state
+//   handleBurgerClear = () => {
+//     this.setState({
+//       clickedIngredients: []
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div style={{ display: 'flex', margin: '3rem', alignItems: 'flex-end' }}>
+//         {/* will recieve ingredients a props and render them, and a event handler to handle ingredient clicks */}
+//         <IngredientList 
+//           ingredients={ingredients}
+//           handleIngredientClick={this.handleIngredientClick}
+//         />
+
+//         {/* will recive the clear button event handler, and the clicked ingredients from state to render */}
+//         <BurgerPane 
+//           clickedIngredients={this.state.clickedIngredients}
+//           handleBurgerClear={this.handleBurgerClear}
+//         />
+//       </div>
+//     )
+//   }
+// }
--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Ingredient extends Component {
-    render() {
-        const styles = {
-            backgroundColor: this.props.ingredient.color,
-            color: this.props.ingredient.color === '#3F250B' ? 'white' : 'black' 
-        }
-
-        return (
-            <div 
-                style={styles}
-                onClick={ this.props.handleIngredientClick ?
-                    () => this.props.handleIngredientClick(this.props.ingredient) :
-                    () => {}
-                }
-            >
-                <p>{this.props.ingredient.name}</p>
-            </div>
-        )
+export default function Ingredient(props) {
+    const styles = {
+        backgroundColor: props.ingredient.color,
+        color: props.ingredient.color === '#3F250B' ? 'white' : 'black' 
     }
+
+
+    return (
+        <div 
+        style={styles}
+        onClick={ props.handleIngredientClick ?
+                () => props.handleIngredientClick(props.ingredient) :
+                () => {}
+            }
+        >
+            <p>{props.ingredient.name}</p>
+        </div>
+    )
 }
+
+// export default class Ingredient extends Component {
+//     render() {
+//         const styles = {
+//             backgroundColor: this.props.ingredient.color,
+//             color: this.props.ingredient.color === '#3F250B' ? 'white' : 'black' 
+//         }
+
+//         return (
+//             <div 
+//                 style={styles}
+//                 onClick={ this.props.handleIngredientClick ?
+//                     () => this.props.handleIngredientClick(this.props.ingredient) :
+//                     () => {}
+//                 }
+//             >
+//                 <p>{this.props.ingredient.name}</p>
+//             </div>
+//         )
+//     }
+// }
