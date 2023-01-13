c546c351b9e235e6af01e14f8dc06314d1d41e10
YOUR-USERNAME <gtellez07@yahoo.com>
2023-01-13 12:12:49 -0600

    Gonzalo T deliverable

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -3,8 +3,13 @@ import React, { Component } from "react";
 export default class Ingredient extends Component {
     render() {
         return (
-            <div onClick={this.props.handleAddToStack}>
+            <div style={{
+                backgrounColor:this.props.item.color,
+                width: '200px',
+                height: '20px'}}
+                onClick={this.props.handleAddToStack}>
                 {this.props.item.name}
+
             </div>
         )
     }
