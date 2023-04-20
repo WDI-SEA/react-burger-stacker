7c6df900a4ce9ecee5a16c778b7ac6e369eb28ce
Heiner000 <garrett.heiner@gmail.com>
2023-04-19 23:05:30 -0700

    ingred still don't stack

--- src/BurgerPane.jsx
+++ src/BurgerPane.jsx
@@ -4,7 +4,7 @@ import BurgerStack from './BurgerStack'
 export default class BurgerPane extends Component {
     render() {
         return(
-            <div>
+            <div style={{border: 'solid 2px green'}} >
                 <BurgerStack burgerStack={this.props.burgerStack} />
                 <button onClick={this.props.onClearBurger} >Clear Burger</button>
             </div>
--- src/BurgerStack.jsx
+++ src/BurgerStack.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class BurgerStack extends Component {
     render() {
         return (
-            <div style={{border: 'solid 2px green', display: 'flex'}} >
+            <div  >
                 {this.props.burgerStack}
             </div>
         )
